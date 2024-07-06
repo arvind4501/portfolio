f6e251cee9ecf83a708d73d5aee3b159f5da2121
arvind4501 <arvindjangirlpu@gmail.com>
2024-07-06 18:40:46 +0530

    updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @projects = Project.all
+
+    if params[:section]
+      render turbo_stream: turbo_stream.replace('main-content', partial: "home/#{params[:section]}")
+    end
   end
-end
+
+  def contact
+    @name = params[:name]
+    @message = params[:message]
+
+    # Example: Send an email or save the contact request to the database
+    # ContactMailer.with(name: @name, email: params[:email], message: @message).deliver_now
+
+    flash[:notice] = "Thank you for your message! will contact you back soon"
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "Thank you for your message! will contact you back soon" }
+      format.turbo_stream { render turbo_stream: turbo_stream.replace('flash', partial: 'layouts/flash') }
+    end
+  end
+end 
